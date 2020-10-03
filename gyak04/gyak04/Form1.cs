1afec42a2de66f005ea32c03e0a95f6cce1f15ed
HI0OMM <xxak99@gmail.com>
2020-10-03 12:00:47 +0200

    feladat7

--- gyak04/gyak04/Form1.cs
+++ gyak04/gyak04/Form1.cs
@@ -7,29 +7,137 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace gyak04
 {
-    using Excel = Microsoft.Office.Interop.Excel;
-    using System.Reflection;
+   
     public partial class Form1 : Form
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
 
-        
+        string[] headers = new string[]
+        {
+             "Kód",
+             "Eladó",
+             "Oldal",
+             "Kerület",
+             "Lift",
+             "Szobák száma",
+             "Alapterület (m2)",
+             "Ár (mFt)",
+             "Négyzetméter ár (Ft/m2)"
+
+         };
+
+        private Excel.Application xlApp;
+        private Excel.Workbook xlWB;
+        private dynamic xlSheet;
+
+
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+
+            
+
+            try
+            {
+                
+                xlApp = new Excel.Application();
+
+                
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                
+                xlSheet = xlWB.ActiveSheet;
+
+                
+                CreateTable(); 
+
+                
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
             
         }
 
-        private void LoadData()
+        void LoadData()
         {
             Flats = context.Flats.ToList();
         }
 
-        
+        void CreateTable()
+        {
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            for (int col = 1; col <= headers.Length; col++)
+            {
+                xlSheet.Cells[1, col] = headers[col - 1];
+            }
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                string lift = "";
+                if (f.Elevator == true)
+                {
+                    lift = "Van";
+                }
+                else
+                {
+                    lift = "Nincs";
+                }
+                values[counter, 4] = lift;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+                xlSheet.get_Range(GetCell(2, 1), GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+            }
+
+           
+
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
+
+
+
     }
 }
