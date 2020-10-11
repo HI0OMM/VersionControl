a6a338a83cf84e6e34121afbf11ec554ee98ea18
HI0OMM <xxak99@gmail.com>
2020-10-11 21:58:09 +0200

    utolso

--- gyak5/gyak5/Form1.cs
+++ gyak5/gyak5/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -64,6 +65,15 @@ namespace gyak5
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            
         }
     }
 }
