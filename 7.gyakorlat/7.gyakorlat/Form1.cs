1e0faca9a3496a0861be702582b9c97a83d25159
HI0OMM <xxak99@gmail.com>
2020-10-24 11:43:40 +0200

    feladat5

--- 7.gyakorlat/7.gyakorlat/Form1.cs
+++ 7.gyakorlat/7.gyakorlat/Form1.cs
@@ -15,6 +15,8 @@ namespace _7.gyakorlat
     
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234);
+
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<Entities.BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
