33b864714c31b4d74f46d41dbc108b7be84a6a7e
HI0OMM <xxak99@gmail.com>
2020-11-12 15:12:52 +0100

    feladat8

--- gyakrlt8/gyakrlt8/Form1.cs
+++ gyakrlt8/gyakrlt8/Form1.cs
@@ -44,7 +44,7 @@ namespace gyakrlt8
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
